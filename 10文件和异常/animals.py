6c6232d4ded535184d4fb66c82081f15e07606c7
Ken <33713393+LieKeWu@users.noreply.github.com>
2022-10-23 23:14:55 +0800

    友好消息/跳过版本

--- 10文件和异常/animals.py
+++ 10文件和异常/animals.py
@@ -1,11 +1,14 @@
-filenames = ['file_text/dogs.txt', 'file_text/cats.txt']
+filenames = ['file_text/dogs.txt', 'cats.txt']
 
 for filename in filenames:
     try:
         with open(filename) as f_obj:
             contents = f_obj.read()
     except FileNotFoundError:
-        pass
+        # 一言不发
+        # pass
+        # 友好消息
+        print("\nFile not found error: " + filename)
     else:
         print("\nReading file:" + filename)
         print(contents)
