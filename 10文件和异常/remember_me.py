f7d05ca1d9bcb87f1cbbca4e9b9391fc06433a77
Ken <33713393+LieKeWu@users.noreply.github.com>
2022-10-26 11:54:04 +0800

    “这里没有任何新代码，只是将前两个示例的代码合并到了一个程序中。在❶处，我们尝试打开文件username.json。如果这个文件存在，就将其中的用户名读取到内存中（见❷），再执行else代码块，即打印一条欢迎用户回来的消息。用户首次运行这个程序时，文件username.json不存在，将引发FileNotFoundError异常（见❸），因此Python将执行except代码块：提示用户输入其用户名（见❹），再使用json.dump()存储该用户名，并打印一句问候语（见❺）。 ”
    Excerpt From
    图灵程序设计丛书：Python 高手进阶之路（套装全10册）
    Jan Erik Solem
    https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewBook?id=0
    This material may be protected by copyright.

--- 10文件和异常/remember_me.py
+++ 10文件和异常/remember_me.py
@@ -1,9 +1,16 @@
 import json
 
-username = input("What is your name?")
+# 如果用户已经存在，直接问候,否则加入文件。
+
 
 filename = 'username.json'
-
-with open(filename, 'w') as f_obj:
-    json.dump(username, f_obj)
-    print("We'll remember you when you come back, " + username + "!")
+try:
+    with open(filename) as f_obj:
+        name = json.load(f_obj)
+except FileNotFoundError:
+    name = input("What is your name?")
+    with open(filename, 'w') as f_obj:
+        json.dump(name, f_obj)
+        print("We'll remember you when you com back, " + name + "!")
+else:
+    print('Welcome ' + name + "!")
