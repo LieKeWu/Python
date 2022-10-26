efdbb7dbe5c245f81e4ff2395e206bf98dc3dafe
Ken <33713393+LieKeWu@users.noreply.github.com>
2022-10-26 11:56:40 +0800

    重构代码
    问候用户函数

--- 10文件和异常/remember_me.py
+++ 10文件和异常/remember_me.py
@@ -1,16 +1,20 @@
 import json
 
+
 # 如果用户已经存在，直接问候,否则加入文件。
 
+def greet_user():
+    """问候用户，并指出其姓名"""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            name = json.load(f_obj)
+    except FileNotFoundError:
+        name = input("What is your name?")
+        with open(filename, 'w') as f_obj:
+            json.dump(name, f_obj)
+            print("We'll remember you when you com back, " + name + "!")
+    else:
+        print('Welcome ' + name + "!")
 
-filename = 'username.json'
-try:
-    with open(filename) as f_obj:
-        name = json.load(f_obj)
-except FileNotFoundError:
-    name = input("What is your name?")
-    with open(filename, 'w') as f_obj:
-        json.dump(name, f_obj)
-        print("We'll remember you when you com back, " + name + "!")
-else:
-    print('Welcome ' + name + "!")
+greet_user()
