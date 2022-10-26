665aaed64237d47557da283efea3bc6396405e30
Ken <33713393+LieKeWu@users.noreply.github.com>
2022-10-26 12:25:05 +0800

    重构练习

--- 10文件和异常/remember_me.py
+++ 10文件和异常/remember_me.py
@@ -3,18 +3,35 @@ import json
 
 # 如果用户已经存在，直接问候,否则加入文件。
 
-def greet_user():
-    """问候用户，并指出其姓名"""
+def get_stored_username():
+    """如果存储了用户名，就获取它"""
     filename = 'username.json'
     try:
         with open(filename) as f_obj:
             name = json.load(f_obj)
     except FileNotFoundError:
-        name = input("What is your name?")
-        with open(filename, 'w') as f_obj:
-            json.dump(name, f_obj)
-            print("We'll remember you when you com back, " + name + "!")
+        return None
     else:
-        print('Welcome ' + name + "!")
+        return name
+
+
+def get_new_username():
+    """提示用户输入用户名"""
+    username = input("What's your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
+
+def greet_user():
+    """问候用户，并指出其姓名"""
+    name = get_stored_username()
+    if name:
+        print('Welcom back, ' + name + "!")
+    else:
+        name = get_new_username()
+        print("We'll remember you when you com back, " + name + "!")
+
 
 greet_user()
