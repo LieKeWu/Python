9bae9456a9f6c2bc76ac1dc0e5c1a8846f73c227
Ken <33713393+LieKeWu@users.noreply.github.com>
2022-10-26 22:13:19 +0800

    创建设置类，并且应用

--- alien_invision/alien_invision.py
+++ alien_invision/alien_invision.py
@@ -1,16 +1,15 @@
 import sys
-
 import pygame
+from setting import Settings
 
 
 def run_game():
     """初始化游戏并创建一个屏幕对象"""
     pygame.init()  # 初始化背景设置
-    screen = pygame.display.set_mode((1200, 800))  # 设置宽高
+    ai_setting = Settings()
+    screen = pygame.display.set_mode(
+        (ai_setting.screen_width, ai_setting.screen_hight))  # 设置宽高
     pygame.display.set_caption("Alien Invision")  # 设置应用标题
-
-    # 设置背景色
-    bg_color = (230, 230, 230)
 
     # 开启游戏住循环
     while True:
@@ -21,7 +20,7 @@ def run_game():
                 sys.exit()
 
         # 每次循环都重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_setting.bg_color)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
